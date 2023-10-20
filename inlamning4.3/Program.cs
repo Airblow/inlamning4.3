7fa39b4830794599afe5319197a4cb9eb21d4ba4
Airblow <143172177+Airblow@users.noreply.github.com>
2023-10-20 13:20:44 +0200

    Add project files.

---
+++ inlamning4.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace inlamning4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int digit=0; digit <= 100; digit++)
+            {
+                if (digit % 3 == 0 && digit % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                }
+                else if (digit % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+
+                }
+                else if(digit % 3==0)
+                {
+                    Console.WriteLine("Fizz");
+
+                }
+                else
+                {
+                    Console.WriteLine(digit);
+                }
+ 
+            }
+
+        }
+
+    }
+
+}
